075ca8ef6876ea41de7c40c1d6f8368a17ae1f3d
duanjinqiu <471568720@qq.com>
2022-08-09 00:49:19 +0800

    重新提交

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
-const http = require('http');
-const httpServer = http.createServer((req, res) => {
-    res.setHeader('Content-Type', 'text/html;charset=utf-8;');
-    res.end('我是好人');
-});
-
-httpServer.listen(3009, () => {
-    console.log('3009……');
-});
+const generateConfig = require('./generateConfig')
+async function start() {
+  // 如果需要手动修改anonymous_token，需要注释generateConfig调用
+  await generateConfig()
+  require('./server').serveNcmApi({
+    checkVersion: true,
+  })
+}
+start()

a6f51f369bef909a0a8c0035ecea30a42bc5bddf
duanjinqiu <471568720@qq.com>
2022-08-09 00:58:07 +0800

    缩小排查范围

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const httpServer = http.createServer((req, res) => {
     res.setHeader('Content-Type', 'text/html;charset=utf-8;');
-    res.end('app.js');
+    res.end('我是bin下的app.js，葫芦娃giao');
 });
 
 httpServer.listen(3009, () => {
--- main.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-const httpServer = http.createServer((req, res) => {
-    res.setHeader('Content-Type', 'text/html;charset=utf-8;');
-    res.end('main.js');
-});
-
-httpServer.listen(3009, () => {
-    console.log('3009……');
-});

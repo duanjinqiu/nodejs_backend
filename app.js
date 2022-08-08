01bda7fd3ea0999376c97578c1b7388ca796b7de
duanjinqiu <471568720@qq.com>
2022-08-09 01:37:15 +0800

    修改服务端返回的内容

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 const httpServer = http.createServer((req, res) => {
     res.setHeader('Content-Type', 'text/html;charset=utf-8;');
     res.setHeader('X-My-Giao', `I'm Giao bro!!!`);
-    res.end('我是bin下的app.js，葫芦娃giao');
+    res.end('vercel.json src必须是index.js，这种规定很奇怪啊');
 });
 
 httpServer.listen(3009, () => {
